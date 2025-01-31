10401ada28c3cbbb2cca357284e94551383af549
lordran <lordran@lordran.dev>
2025-01-31 00:18:38 -0600

    Account

--- bank/account.py
+++ bank/account.py
@@ -3,10 +3,35 @@ from colorama import Fore, Style
 
 class Account:
 
-    def __init__(self, name: str, password: str):
+    def __init__(self, name: str, balance: float, password: str):
         self.name = name
+        self.balance = balance
         self.password = password
+
+    def deposit(self, amount: float, password: str) -> float | None:
+        if password != self.password:
+            return None
+        if amount < 0:
+            return None
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount: float, password: str) -> float | None:
+        if password != self.password:
+            return None
+        if amount > self.balance:
+            return None
+        if amount < 0:
+            return None
+        self.balance -= amount
+        return self.balance
+
+    def get_balance(self, password : str) -> float | None:
+        if password != self.password:
+            return None
+        return self.balance
 
     def show(self):
         print(f"{Fore.RED} Name: {self.name}")
+        print(f"{Fore.CYAN} Balance: {self.balance}")
         print(f"{Fore.BLUE} Password: {self.password} {Style.RESET_ALL}")
--- main.py
+++ main.py
@@ -2,9 +2,63 @@ from bank.account import Account
 
 
 def main():
-    print("Some UV")
-    account = Account("Lordran", "Password")
-    account.show()
+
+    accounts : dict[int, Account] = {}
+    next_account_number = 0
+
+    print("Bank account system")
+    print("Press b to get the balance")
+    print("Press d to make a deposit")
+    print("Press o to open a new account")
+    print("Press w to make a withdrawal")
+    print("Press s to show all accounts")
+    print("Press q to quit")
+
+    action = input("What would you like to do? ").lower()[0]
+    match action:
+        case "b":
+            print("Getting balance")
+            account_number = int(input("Enter account number: "))
+            password = input("Enter password: ")
+            account = accounts[account_number]
+            balance = account.get_balance(password)
+            if balance is not None:
+                print("Balance: ${0:.2f}".format(balance))
+
+        case "d":
+            print("Making a deposit")
+            account_number = int(input("Enter account number: "))
+            amount = int(input("Enter amount: "))
+            password = input("Enter password: ")
+            account = accounts[account_number]
+            balance = account.deposit(amount, password)
+            if balance is not None:
+                print("Balance: ${0:.2f}".format(balance))
+
+        case "o":
+            print("Opening a new account")
+            name = input("Enter account name: ")
+            balance = float(input("Enter initial balance: "))
+            password = input("Enter password: ")
+            account = Account(name, balance, password)
+            account.show()
+
+        case "w":
+            print("Withdrawal")
+            account_number = int(input("Enter account number: "))
+            amount = int(input("Enter amount: "))
+            password = input("Enter password: ")
+            account = accounts[account_number]
+            balance = account.withdraw(amount, password)
+            print(f"Balance: ${balance}")
+
+        case "s":
+            print("Showing all accounts")
+            for account in accounts.values():
+                account.show()
+
+        case "q":
+            print("Quit")
 
 
 if __name__ == "__main__":
