5edc32c3f3cd6cc8c044dd4060951e51be4be754
lordran <lordran@lordran.dev>
2025-02-16 15:11:33 -0600

    Added CLI args for spawning more balls

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,25 @@
+import argparse
 import game
 
 
 def main():
-    game.run()
+    parser = argparse.ArgumentParser(
+        prog="Ball Game",
+        description="Ball game for entertaining purposes",
+    )
+
+    # Add an argument for the number of balls
+    parser.add_argument(
+        "-n", "--num-balls",
+        type=int,
+        default=1,  # Default number of balls
+        help="Number of balls to spawn in the game (default: 50)"
+    )
+
+    args = parser.parse_args()
+
+    # Pass the number of balls to the game.run() function
+    game.run(num_balls=args.num_balls)
 
 
 if __name__ == "__main__":
